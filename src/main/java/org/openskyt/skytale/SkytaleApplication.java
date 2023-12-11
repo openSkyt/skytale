ceaaa37bbaca78a07ba22f0b4122cf34a7ce6737
Marek Lochman <lochman59@gmail.com>
2023-12-11 11:20:01 +0100

    Initial commit

---
+++ src/main/java/org/openskyt/skytale/SkytaleApplication.java
@@ -0,0 +1,13 @@
+package org.openskyt.skytale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkytaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkytaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/openskyt/skytale/SkytaleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openskyt.skytale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkytaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
