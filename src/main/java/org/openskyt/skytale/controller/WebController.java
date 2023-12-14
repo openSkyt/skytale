04803272e14006300b0eed2f8466b42139832c61
Marek Lochman <lochman59@gmail.com>
2023-12-14 11:54:53 +0100

    First working prototype of the groupchat creation page

--- src/main/java/org/openskyt/skytale/controller/WebController.java
+++ src/main/java/org/openskyt/skytale/controller/WebController.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import org.openskyt.skytale.models.Chatroom;
 import org.openskyt.skytale.security.SecurityService;
 import org.openskyt.skytale.service.ChatroomService;
+import org.openskyt.skytale.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
 
 @Controller
 @AllArgsConstructor
@@ -15,6 +20,7 @@ public class WebController {
 
     private SecurityService securityService;
     private ChatroomService chatroomService;
+    private UserService userService;
 
     @GetMapping("/")
     public String index(Model model) {
@@ -32,4 +38,23 @@ public class WebController {
 
         return "chat";
     }
+
+    @GetMapping("/newgroupchat")
+    public String newGroupChatGET(Model m) {
+        m.addAttribute("users", userService.getAll());
+        return "newGroupChat";
+    }
+
+    @PostMapping("/newgroupchat")
+    public String newGroupChatPOST(String groupName, Long[] groupUsers) {
+        Long id = chatroomService.createRoom(securityService.getLoggedInUser().getId(), groupName).getId();
+        chatroomService.addParticipant(id, securityService.getLoggedInUser().getId()); //adds the owner as a participant
+
+        for (Long i:groupUsers){
+            chatroomService.addParticipant(id, i);
+        }
+
+        return "redirect:/"+id;
+    }
+
 }
